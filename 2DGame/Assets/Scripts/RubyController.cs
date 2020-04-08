a02e4e66121f11fa797fc2d4e2c55bcc6a0e6d2d
Adam Alimi <aalimi.mr@gmail.com>
2020-04-08 17:18:00 +0800

    Scene transition proof of concept

---
+++ 2DGame/Assets/Scripts/RubyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+    public float speed = 3.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        Vector2 position = rigidbody2D.position;
+
+        position = position + move * speed * Time.deltaTime;
+
+        rigidbody2D.MovePosition(position);
+    }
+}
