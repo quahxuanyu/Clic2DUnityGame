c468b15bd554bcefb1a17762fa109e2ac4098f8c
Quah XuanYu <xuanthegreat@gmail.com>
2020-09-26 13:10:45 +0800

    MInor change in npcmovement code

--- 2DGame/Assets/Scripts/ServentMoveScript.cs
+++ 2DGame/Assets/Scripts/ServentMoveScript.cs
@@ -69,54 +69,60 @@ public class ServentMoveScript : MonoBehaviour
     void Update()
     {
         if (textBox.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text == activateText)
-        { 
-            currentTargetPoint = new Vector2(Mathf.Round((previosPosition.x + moveSequence[currentMoveSequence].x) * 10f) / 10f, Mathf.Round((previosPosition.y + moveSequence[currentMoveSequence].y) * 10f) / 10f);
-            RigidbodyPosition = new Vector2(Mathf.Round(rigidBody2D.position.x * 10f) / 10f, Mathf.Round(rigidBody2D.position.y * 10f) / 10f);
+        {
+            NPCMovement(moveSequence);
+        }
+    }
 
-            if (RigidbodyPosition == currentTargetPoint)
+    public void NPCMovement(List<Vector2> moveArray)
+    {
+        currentTargetPoint = new Vector2(Mathf.Round((previosPosition.x + moveSequence[currentMoveSequence].x) * 10f) / 10f, Mathf.Round((previosPosition.y + moveSequence[currentMoveSequence].y) * 10f) / 10f);
+        RigidbodyPosition = new Vector2(Mathf.Round(rigidBody2D.position.x * 10f) / 10f, Mathf.Round(rigidBody2D.position.y * 10f) / 10f);
+
+        if (RigidbodyPosition == currentTargetPoint)
+        {
+            if (currentMoveSequence != moveSequence.Count - 1)
             {
-                if (currentMoveSequence != moveSequence.Count - 1)
+                currentMoveSequence++;
+                previosPosition = rigidBody2D.position;
+                currentMoveDirection = new Vector2(0f, 0f);
+                currentTargetPoint = new Vector2(Mathf.Round((previosPosition.x + moveSequence[currentMoveSequence].x) * 10f) / 10f, Mathf.Round((previosPosition.y + moveSequence[currentMoveSequence].y) * 10f) / 10f);
+                RigidbodyPosition = new Vector2(Mathf.Round(rigidBody2D.position.x * 10f) / 10f, Mathf.Round(rigidBody2D.position.y * 10f) / 10f);
+                if (RigidbodyPosition.x - currentTargetPoint.x < 0 && currentTargetPoint.x != 0f)
                 {
-                    currentMoveSequence++;
-                    previosPosition = rigidBody2D.position;
-                    currentMoveDirection = new Vector2(0f, 0f);
-                    currentTargetPoint = new Vector2(Mathf.Round((previosPosition.x + moveSequence[currentMoveSequence].x) * 10f) / 10f, Mathf.Round((previosPosition.y + moveSequence[currentMoveSequence].y) * 10f) / 10f);
-                    RigidbodyPosition = new Vector2(Mathf.Round(rigidBody2D.position.x * 10f) / 10f, Mathf.Round(rigidBody2D.position.y * 10f) / 10f);
-                    if (RigidbodyPosition.x - currentTargetPoint.x < 0 && currentTargetPoint.x != 0f)
-                    {
-                        //Debug.Log("ONE");
-                        currentMoveDirection += new Vector2(1f, 0f);
-                    }
-                    else if (RigidbodyPosition.x - currentTargetPoint.x != 0)
-                    {
-                        //Debug.Log("TWO");
-                        currentMoveDirection += new Vector2(-1f, 0f);
-                    }
+                    //Debug.Log("ONE");
+                    currentMoveDirection += new Vector2(1f, 0f);
+                }
+                else if (RigidbodyPosition.x - currentTargetPoint.x != 0)
+                {
+                    //Debug.Log("TWO");
+                    currentMoveDirection += new Vector2(-1f, 0f);
+                }
 
-                    if (RigidbodyPosition.y - currentTargetPoint.y < 0 && currentTargetPoint.y != 0f)
-                    {
-                        // Debug.Log("THREE");
-                        currentMoveDirection += new Vector2(0f, 1f);
-                    }
-                    else if (RigidbodyPosition.y - currentTargetPoint.y != 0)
-                    {
-                        //Debug.Log("FOUR");
-                        currentMoveDirection += new Vector2(0f, -1f);
-                    }
-                    //Debug.Log("New!!");
-                    // Debug.Log(currentMoveDirection);
-                }
-                else
+                if (RigidbodyPosition.y - currentTargetPoint.y < 0 && currentTargetPoint.y != 0f)
                 {
-                    rigidBody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+                    // Debug.Log("THREE");
+                    currentMoveDirection += new Vector2(0f, 1f);
                 }
+                else if (RigidbodyPosition.y - currentTargetPoint.y != 0)
+                {
+                    //Debug.Log("FOUR");
+                    currentMoveDirection += new Vector2(0f, -1f);
+                }
+                //Debug.Log("New!!");
+                // Debug.Log(currentMoveDirection);
             }
             else
             {
-                //Debug.Log("MOVING");
-                // Debug.Log(currentMoveDirection);
-                rigidBody2D.MovePosition(rigidBody2D.position + currentMoveDirection * 2f * Time.deltaTime);
+                rigidBody2D.constraints = RigidbodyConstraints2D.FreezeAll;
             }
         }
-    }   
+        else
+        {
+            //Debug.Log("MOVING");
+            // Debug.Log(currentMoveDirection);
+            rigidBody2D.MovePosition(rigidBody2D.position + currentMoveDirection * 2f * Time.deltaTime);
+        }
+    }
 }
+
