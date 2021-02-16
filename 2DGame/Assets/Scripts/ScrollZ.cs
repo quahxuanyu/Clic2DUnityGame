0b54d630963206be3af786e0c5e0df83dffdd73c
Surya Ismail <suryaismail@yahoo.com>
2021-02-16 10:05:49 +0800

    ENding scene

---
+++ 2DGame/Assets/Scripts/ScrollZ.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollZ : MonoBehaviour
+{
+    public float endOfText = 0;
+    private static bool isScrolling = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isScrolling) {
+            return;
+        }
+
+        Vector3 pos = transform.position;
+
+        Vector3 localVectorUp = transform.TransformDirection(0, 0.25f, 0);
+        pos += localVectorUp;
+
+        transform.position = pos;
+
+        if (transform.position.y > endOfText)
+        {
+          isScrolling = false;
+        }
+    }
+}
